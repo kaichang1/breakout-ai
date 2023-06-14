55f6f0500caabd873d7bdf8ac066cc1cdf416a1c
Kai <kaichang92@gmail.com>
2023-06-14 15:49:19 -0700

    Add mute button to all scenes, update prefabs

--- Assets/Scripts/Mute.cs
+++ Assets/Scripts/Mute.cs
@@ -1,60 +1,3 @@
-//using UnityEngine;
-//using UnityEngine.UI;
-
-//public class Mute : MonoBehaviour
-//{
-//    public Sprite musicOnSprite;
-//    public Sprite musicOnSelectedSprite;
-
-//    public Sprite musicOffSprite;
-//    public Sprite musicOffSelectedSprite;
-
-//    private Image image;
-//    private Button button;
-
-//    private void Awake()
-//    {
-//        image = GetComponent<Image>();
-//        button = GetComponent<Button>();
-//    }
-
-//    public void Toggle()
-//    {
-//        if (GameSettings.Instance.muted)
-//        {
-//            UnmuteAudio();
-
-//            GameSettings.Instance.muted = false;
-//            image.sprite = musicOnSprite;
-//            // Change Highlited Sprite
-//            SpriteState ss = button.spriteState;
-//            ss.highlightedSprite = musicOnSelectedSprite;
-//            button.spriteState = ss;
-//        }
-//        else
-//        {
-//            MuteAudio();
-
-//            GameSettings.Instance.muted = true;
-//            image.sprite = musicOffSprite;
-//            // Change Highlited Sprite
-//            SpriteState ss = button.spriteState;
-//            ss.highlightedSprite = musicOffSelectedSprite;
-//            button.spriteState = ss;
-//        }
-//    }
-
-//    public void MuteAudio()
-//    {
-//        AudioListener.pause = true;
-//    }
-
-//    public void UnmuteAudio()
-//    {
-//        AudioListener.pause = false;
-//    }
-//}
-
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -64,6 +7,18 @@ public class Mute : MonoBehaviour
 
     public Sprite musicOnSprite;
     public Sprite musicOffSprite;
+
+    private void Start()
+    {
+        if (GameSettings.Instance.muted)
+        {
+            icon.sprite = musicOffSprite;
+        }
+        else
+        {
+            icon.sprite = musicOnSprite;
+        }
+    }
 
     public void Toggle()
     {
